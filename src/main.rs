645306b0ba70264b54a8c32b693180d662d411f1
John Bannick <jbannick@7128.com>
2024-08-06 05:37:38 -0400

    Added documentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+//! This app is a prototype Tactical Microgrid System implementation written in Rust.
+//!
+//! It publishes TMS Heartbeats from a TMS-enabled Device, such as a Generator (genset)
+//! and receives those messages at a TMS-enabled Dashboard.
+//!
+//! It uses an Object Management Group, Data Distribution Service library implemented
+//! in pure Rust (no C or C++) by Atostek Oy, a Finnish company.
+//! See: https://quicksearch.dla.mil/qsDocDetails.aspx?ident_number=285095
+//! See: https://www.omg.org/omg-dds-portal/
+//! See: https://github.com/jhelovuo/RustDDS
+//!
+//! Usage:
+//!   cargo run -- --servertype [pub | sub]
+//!
+//! How to run:
+//! 
+//! On your Linux, Windows, or MacOS computer
+//! In Terminal Window One
+//!
+//! Step 1. Go to the directory containing the Cargo.toml file for this app.
+//! Step 2. Execute: cargo run -- --servertype sub
+//!         Should start this app as a TMS Subscriber and wait for connections
+//! 
+//! In Terminal Window Two
+//! 
+//! Step 3. Repeat Steps 1. 2 above except execute: cargo run -- --servertype pub
+//!         Should start this app as a TMS Publisher, connect to all Subscribers,
+//!         and begin publishing TMS Heartbeat messages one per second.
+//!         All TMS Subscribers should receive and display those messages.
+//!
+//! You can have multiple Publishers and multiple Subscribers, one Terminal window each.
+//! You can stop and restart Publishers and Subscribers.
+//! You can start and stop Publishers and Subscribers in any order.
+//!
+//! MIT License
+//!
+//! Copyright (c) 2024 John Bannick
+//!
+//! Permission is hereby granted, free of charge, to any person obtaining a copy
+//! of this software and associated documentation files (the "Software"), to deal
+//! in the Software without restriction, including without limitation the rights
+//! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//! copies of the Software, and to permit persons to whom the Software is
+//! furnished to do so, subject to the following conditions:
+//!
+//! The above copyright notice and this permission notice shall be included in all
+//! copies or substantial portions of the Software.
+//!
+//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//! SOFTWARE.
+//!
 #![allow(unused)]
 use std::{io, time};
 use std::fmt;
